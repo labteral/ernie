eaac69373dc266cb8d023db863f5419210a92953
MarcosFP97 <marcosfernandezpichel@gmail.com>
2020-06-19 22:59:37 +0200

    Minor fix

--- test/load_csv.py
+++ test/load_csv.py
@@ -6,7 +6,7 @@ from ernie import SentenceClassifier, Models
 
 class TestLoadCsv(unittest.TestCase):
     classifier = SentenceClassifier(model_name=Models.BertBaseUncased, max_length=128, labels_no=2)
-    classifier.load_dataset(validation_split=0.2,csv_path="example.csv",header=None)
+    classifier.load_dataset(validation_split=0.2,csv_path="example.csv",read_csv_kwargs={"header":None})
     classifier.fine_tune(epochs=4, learning_rate=2e-5, training_batch_size=32, validation_batch_size=64)
 
     def test_predict(self):
